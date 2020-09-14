83bec287c1e7212282312e9907d52284ad2f9b47
Pranav Kulshrestha <prkulshrestha16@gmail.com>
2020-09-14 23:23:45 +0530

    Adding other solution of loot houses

--- DynammicProgramming/loot_houses.cpp
+++ DynammicProgramming/loot_houses.cpp
@@ -8,17 +8,20 @@
 //        2) we have looted houses, we do not want to loot this house hence, this possiblity is dp[i-1] which will get maximum loot upto i-1
 //        3) we have looted houses, we want to loot this house hence, we will have to add current money(arr[i]) to dp[i-2]
 
-#include <bits/stdc++.h>
-using namespace std;
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n=nums.size();
+        vector<int> dp(n,0);
+        if(n<=0) return 0;
+        if(n==1) return nums[0];
+        if(n==2) return max(nums[0], nums[1]);
+        dp[0]=nums[0];
+        dp[1]=max(nums[0], nums[1]);
 
-int getMaxMoney(int arr[], int n){
-    int dp[n];
-    dp[0] = arr[0];
-    dp[1] = max(arr[0], arr[1]);
-
-    for(int i=2; i<n; i++) {
-        dp[i] = max(max(arr[i], dp[i-1]), dp[i-2] + arr[i]);
+        for(int i=2; i<n; i++) {
+            dp[i] = max(dp[i-1], dp[i-2]+nums[i]);
+        }
+        return dp[n-1];
     }
-
-	return dp[n-1];
-}
+};
